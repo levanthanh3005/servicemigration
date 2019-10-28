59618aae8caeae8dfd2dd9c825117eeb576a589d
Van Thanh Le <vanthanhle@Vans-MacBook-Pro.local>
2019-10-28 11:02:46 +0100

    Ad

--- SM/server.js
+++ SM/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const extras = require('../extras');
+const extras = require('./extras');
 
 const bodyParser = require('body-parser');
 const request = require('request');
