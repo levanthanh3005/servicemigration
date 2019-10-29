ef1a8d9d3dab82e31dbe5c422b20885d8c456418
Van Thanh Le <vanthanhle@Vans-MacBook-Pro.local>
2019-10-29 15:26:47 +0100

    Ad

--- MEC/server.js
+++ MEC/server.js
@@ -447,7 +447,7 @@ function getRunningContainers(callback){
     var cmd = "docker inspect "+runningContainers[index].id;
     // console.log("Run:"+cmd);
     extras.execute(cmd, function(stdout) {
-      console.log(stdout);
+      // console.log(stdout);
       var inspectData = JSON.parse(stdout)[0];
       var lsPortData = inspectData["NetworkSettings"]["Ports"];
       var lsPorts = [];
