be37472e98787d0a5b393a3e92156fa6f94ce614
Van Thanh Le <vanthanhle@Vans-MacBook-Pro.local>
2020-03-22 16:25:05 +0100

    AD

--- Runc/MEC-Runc/script/lazyPageRestore.sh
+++ Runc/MEC-Runc/script/lazyPageRestore.sh
@@ -8,9 +8,10 @@ chmod -R 777 *
 
 cd $HOME/containerroots/$NAME/image
 
-criu lazy-pages --page-server --address $SOURCEADDRESS --port 27 -D checkpoint -vv </dev/null &>/dev/null &
+#criu lazy-pages --page-server --address $SOURCEADDRESS --port 27 -D checkpoint -vv </dev/null &>/dev/null &
+$(criu lazy-pages --page-server --address $SOURCEADDRESS --port 27 -D checkpoint &) &
 
-sleep 3
+#sleep 3
 #criu lazy-pages --page-server --address 172.17.0.3 --port 27 -D checkpoint -vv
 runc restore -d --work-path checkpoint --image-path checkpoint --lazy-pages $NAME
 #runc restore -d --work-path checkpoint --image-path checkpoint --lazy-pages videoserver
